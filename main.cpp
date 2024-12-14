e667e8aa000845d2c6e22ef97ba43ac01a176da9
Supreme Intelligence <supremeintelligence1602@mail.ru>
2024-12-15 01:00:58 +0300

    Implemented the logic of MinMax thread. Added struct ThreadData for secure data exchange between threads.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
 ﻿// OS_Lab2.cpp: определяет точку входа для приложения.
 //
 
-#include "OS_Lab2.h"
 #include<iostream>
+#include<vector>
 #include<windows.h>
 
 using namespace std;
 
-DWORD WINAPI min_max()
+struct ThreadData
 {
+	vector<int>arr;
+	int minVal, maxVal;
+	double average;
+
+	ThreadData(int size) : arr(size), minVal(0), maxVal(0), average(0.0) {};
+};
+
+DWORD WINAPI min_max(LPVOID lpParameter)
+{
+	ThreadData* data = static_cast<ThreadData*>(lpParameter);
+	data->minVal = data->arr[0];
+	data->maxVal = data->arr[0];
+
+	for (int i = 0; i < data->arr.size(); i++)
+	{
+		if (data->arr[i] < data->minVal)	data->minVal = data->arr[i];
+		else								data->maxVal = data->arr[i];
+
+		Sleep(7);
+	}
 	return 0;
 }
 
-DWORD WINAPI average()
+DWORD WINAPI average(LPVOID lpParameter)
 {
 	return 0;
 }
@@ -22,22 +42,46 @@ int main()
 	int n;
 	cout << "Enter the size of array:\t";
 	cin >> n;
-	int* arr = new int [n];
+	ThreadData* data = new ThreadData(n);
 	cout << "Enter " << n << " elements:\t";
 	for (int i = 0; i < n; i++)
 	{
-		cin >> arr[i];
+		cin >> data->arr[i];
 		cout << "\t";
 	}
 	cout << endl;
+	
+	HANDLE hMinMax, hAverage;
+	DWORD IDMinMax, IDAverage;
 
-	HANDLE hThread;
-	DWORD IDThread;
-
-	hThread = CreateThread(NULL, 0, nullptr, nullptr, 0, &IDThread);
-	if (hThread == nullptr)
+	hMinMax = CreateThread(NULL, 0, min_max, data, 0, &IDMinMax);
+	if (hMinMax == nullptr)
 	{
+		cout << "Error generating thread MinMax\n";
 		return GetLastError();
 	}
+	else
+	{
+		cout << "Thread MinMax created\n";
+	}
+
+	hAverage = CreateThread(NULL, 0, average, data, 0, &IDAverage);
+	if (hMinMax == nullptr)
+	{
+		cout << "Error generating thread Avreage\n";
+		return GetLastError();
+	}
+	else
+	{
+		cout << "Thread Average created\n";
+	}
+
+	WaitForSingleObject(hMinMax, INFINITE);
+	WaitForSingleObject(hAverage, INFINITE);
+
+
+	CloseHandle(hMinMax);
+	CloseHandle(hAverage);
+	cout << "Min array element: \t" << data->minVal << "\nMax array element:\t" << data->maxVal << endl;
 	return 0;
 }
