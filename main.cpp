6c2aa26e7ab49f7deb71f0cbdc6f53492724d3aa
Supreme Intelligence <supremeintelligence1602@mail.ru>
2024-12-15 18:46:35 +0300

    Redesigned file hierarchy. Thread functions moved to Threads library. Configured CMakeLists.txt files.

--- main.cpp
+++ main.cpp
@@ -4,77 +4,10 @@
 #include<iostream>
 #include<vector>
 #include<windows.h>
+#include<ThreadData.h>
+#include<threads.h>
 
 using namespace std;
-
-struct ThreadData
-{
-	vector<int>arr;
-	int minVal, maxVal;
-	double average;
-
-	ThreadData(int size) : minVal(-1), maxVal(-1), average(0.0) 
-	{
-		if (size <= 0)
-		{
-			arr.resize(0);
-		}
-	};
-};
-
-DWORD WINAPI min_max(LPVOID lpParameter)
-{
-	try
-	{
-		ThreadData* data = static_cast<ThreadData*>(lpParameter);
-		if (data->arr.empty())
-		{
-			throw runtime_error("Calculating error: The array is empty.");
-		}
-		data->minVal = data->arr[0];
-		data->maxVal = data->arr[0];
-
-		for (int i = 0; i < data->arr.size(); i++)
-		{
-			if (data->arr[i] < data->minVal)	data->minVal = data->arr[i];
-			else								data->maxVal = data->arr[i];
-
-			Sleep(7);
-		}
-		cout << "Min array element: \t" << data->minVal << "\nMax array element:\t" << data->maxVal << endl;
-	}
-	catch (const exception& e)
-	{
-		cout << "ERROR: " << e.what() << endl;
-		return 1;
-	}
-	return 0;
-}
-
-DWORD WINAPI average(LPVOID lpParameter)
-{
-	try
-	{
-		ThreadData* data = static_cast<ThreadData*>(lpParameter);
-		if (data->arr.empty())
-		{
-			throw runtime_error("Division by zero: The array is empty.");
-		}
-		for (int i = 0; i < data->arr.size(); i++)
-		{
-			data->average += data->arr[i];
-			Sleep(12);
-		}
-		data->average = data->average / data->arr.size();
-		cout << "Average value of array elements:\t" << data->average << endl;
-	}
-	catch (const exception& e)
-	{
-		cout << "ERROR: " << e.what() << endl;
-		return 1;
-	}
-	return 0;
-}
 
 int main()
 {
