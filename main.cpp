4831fb356ed891564da5da499f90b7c15a27f4d7
Supreme Intelligence <supremeintelligence1602@mail.ru>
2024-12-15 17:47:06 +0300

    Completed the implemention of the logic of Main thread. Added exception handling.

--- main.cpp
+++ main.cpp
@@ -13,36 +13,66 @@ struct ThreadData
 	int minVal, maxVal;
 	double average;
 
-	ThreadData(int size) : arr(size), minVal(-1), maxVal(-1), average(0.0) {};
+	ThreadData(int size) : minVal(-1), maxVal(-1), average(0.0) 
+	{
+		if (size <= 0)
+		{
+			arr.resize(0);
+		}
+	};
 };
 
 DWORD WINAPI min_max(LPVOID lpParameter)
 {
-	ThreadData* data = static_cast<ThreadData*>(lpParameter);
-	data->minVal = data->arr[0];
-	data->maxVal = data->arr[0];
-
-	for (int i = 0; i < data->arr.size(); i++)
+	try
 	{
-		if (data->arr[i] < data->minVal)	data->minVal = data->arr[i];
-		else								data->maxVal = data->arr[i];
+		ThreadData* data = static_cast<ThreadData*>(lpParameter);
+		if (data->arr.empty())
+		{
+			throw runtime_error("Calculating error: The array is empty.");
+		}
+		data->minVal = data->arr[0];
+		data->maxVal = data->arr[0];
 
-		Sleep(7);
+		for (int i = 0; i < data->arr.size(); i++)
+		{
+			if (data->arr[i] < data->minVal)	data->minVal = data->arr[i];
+			else								data->maxVal = data->arr[i];
+
+			Sleep(7);
+		}
+		cout << "Min array element: \t" << data->minVal << "\nMax array element:\t" << data->maxVal << endl;
 	}
-	cout << "Min array element: \t" << data->minVal << "\nMax array element:\t" << data->maxVal << endl;
+	catch (const exception& e)
+	{
+		cout << "ERROR: " << e.what() << endl;
+		return 1;
+	}
 	return 0;
 }
 
 DWORD WINAPI average(LPVOID lpParameter)
 {
-	ThreadData* data = static_cast<ThreadData*>(lpParameter);
-
-	for (int i = 0; i < data->arr.size(); i++)
+	try
 	{
-		data->average += data->arr[i];
-		Sleep(12);
+		ThreadData* data = static_cast<ThreadData*>(lpParameter);
+		if (data->arr.empty())
+		{
+			throw runtime_error("Division by zero: The array is empty.");
+		}
+		for (int i = 0; i < data->arr.size(); i++)
+		{
+			data->average += data->arr[i];
+			Sleep(12);
+		}
+		data->average = data->average / data->arr.size();
+		cout << "Average value of array elements:\t" << data->average << endl;
 	}
-	cout << "Average value of array elements:\t" << data->average << endl;
+	catch (const exception& e)
+	{
+		cout << "ERROR: " << e.what() << endl;
+		return 1;
+	}
 	return 0;
 }
 
@@ -66,7 +96,7 @@ int main()
 	hMinMax = CreateThread(NULL, 0, min_max, data, 0, &IDMinMax);
 	if (hMinMax == nullptr)
 	{
-		cout << "Error generating thread MinMax\n";
+		cout << "Error generating MinMax thread\n";
 		return GetLastError();
 	}
 	else
@@ -74,12 +104,16 @@ int main()
 		cout << "\n/// Thread MinMax created ///\n\n";
 	}
 	WaitForSingleObject(hMinMax, INFINITE);
-	CloseHandle(hMinMax);
+
+	if (CloseHandle(hMinMax))
+	{
+		cout << "\n///Thread MinMax completed///\n\n";
+	}
 
 	hAverage = CreateThread(NULL, 0, average, data, 0, &IDAverage);
 	if (hAverage == nullptr)
 	{
-		cout << "Error generating thread Avreage\n";
+		cout << "Error generating Average thread\n";
 		return GetLastError();
 	}
 	else
@@ -88,8 +122,19 @@ int main()
 	}
 
 	WaitForSingleObject(hAverage, INFINITE);
-	CloseHandle(hAverage);	
-	
-
+	if (CloseHandle(hAverage))
+	{
+		cout << "\n/// Thread Average completed ///\n\n";
+	}
+	cout << "Final array output:"<<endl<<"[ ";
+	for (int i = 0; i < data->arr.size(); i++)
+	{
+		if (data->arr[i] == data->minVal || data->arr[i] == data->maxVal)
+		{
+			data->arr[i] = data->average;
+		}
+		cout << data->arr[i] << " ";
+	}
+	cout << "]"<<endl;
 	return 0;
 }
